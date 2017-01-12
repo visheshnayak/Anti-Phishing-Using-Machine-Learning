71c14e580bb7b04b83e001653970ddcd252f3461
Vishesh Kumar Nayak <visheshnayak@gmail.com>
2017-01-12 15:09:00 +0530

    Added the feature to write in File

--- capture.py
+++ capture.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+
+#open file in order to write the URLs
+f = open('URL.txt', 'w+')
+
+#Import
 from selenium import webdriver
 
 #Get Which browser the user is using
@@ -9,11 +14,13 @@ url = driver.current_url
 
 #Get the URL every time it changes
 while (True):
-    try:
-        urllater = driver.current_url
-    except selenium.common.exceptions.NoSuchWindowException :
-        print("You have closed the browser.")
+    #try:
+    urllater = driver.current_url
+    #except selenium.NoSuchWindowException as e :
+    #    print("You have closed the browser.")
 
     if url != urllater :
         url = urllater
-        print (url)
+        f.write(url + '\n')
+
+#Close the File
