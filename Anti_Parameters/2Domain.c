7609b1b989e047620122a9ee627e7a55090461ad
RohanJosephh <rohanshaju@yahoo.co.in>
2017-03-11 10:39:59 +0530

    Update 2Domain.c
    Checking the start of URL for domain as well existence of a second domain name in the URL

--- Anti_Parameters/2Domain.c
+++ Anti_Parameters/2Domain.c
@@ -6,6 +6,7 @@ main()
 	char url[100];
 	int w=0;
 	int flag=0;
+	int p=0;
 	//scanf("%s",url);
 	gets(url);
 	for(i=0;i<4;i++)
@@ -22,4 +23,13 @@ main()
 		printf("False");
 	else
 		printf("True");
+	for(i=4;i<strlen(url)-3;i++)
+	{
+		if(url[i]=='w' && url[i+1]=='w' && url[i+2]=='w' && url[i+3]=='.')
+			p=1;
+	}
+	if(p==1)
+		printf("\nTrue");
+	else
+		printf("\nFalse");
 }
