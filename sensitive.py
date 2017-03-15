a33bdc07c648b714ba7ae1bfcc231330bbd6e8b0
ambikamam <ambika.mam12@gmail.com>
2017-03-15 16:19:37 +0530

    Create sensitive.py
    checks for any sensitive words in the url

---
+++ sensitive.py
@@ -0,0 +1,19 @@
+url = raw_input()
+if 'secur' in url :
+ print('1')
+elif 'login' in url:
+ print('1')
+elif 'signin' in url:
+ print('1')
+elif 'webscr' in url:
+ print('1')
+elif 'ebayisapi' in url:
+ print('1')
+elif 'banking' in url:
+ print('1')
+elif 'confirm' in url:
+ print('1')
+elif 'paypal' in url:
+ print('1')
+else:
+ print('0')
