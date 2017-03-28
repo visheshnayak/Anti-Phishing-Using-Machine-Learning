d2c845083e8473e4df09e27914adfd93fb677951
Vishesh Kumar Nayak <visheshnayak@gmail.com>
2017-03-29 01:01:26 +0530

    Relearning the dataset

--- learn.py
+++ learn.py
@@ -1,37 +1,58 @@
 import numpy as np
 from sklearn.svm import SVC
-#import pickle
+import pickle
 from featureAdder import *
 from sklearn.externals import joblib
 
 #The dataset list
 learnlist=[]
 
-#declaring the kernel
-clf = SVC(kernel = 'linear')
+def learning() :
+    #No. of trusted websites
+#    trustedWeb = 502
 
-#reading the list.txt file to create the datasets
-with open('list.txt', 'r') as f:
-    for line in f:
-        learnlist.append(featureAdd(line))
+    #No. of Untrusted websites
+#    untrustedWeb = 2497
 
-#Adding the dataset
-X = np.array(learnlist)
+    #Get the numbers of trusted websites and untrusted websites
+    with open('objs.pickle', 'rb') as f:
+        trustedWeb, untrustedWeb = pickle.load(f)
 
-#Adding the phishing labels
-y = [1]*2497
+    f.close()
 
-#Adding the Non phishing labels
-y.extend([0]*501)
+    #Write the the stats back to the dump
+    trustedWeb += 1
+    with open('objs.pickle', 'wb') as f2:
+        pickle.dump([trustedWeb, untrustedWeb], f2)
+    f2.close()
 
-#finding the pattern
-clf.fit(X, y)
+    #declaring the kernel
+    clf = SVC(kernel = 'linear')
 
-'''
-s = pickle.dumps(clf)
-clf2 = pickle.load(s)
-clf2.predict(featureAdd("https://www.google.com"))
-'''
+    #reading the list.txt file to create the datasets
+    with open('list.txt', 'r') as f:
+        for line in f:
+            learnlist.append(featureAdd(line))
 
-#Saving for later purposes
-joblib.dump(clf, 'learned.pkl')
+    #Adding the dataset
+    X = np.array(learnlist)
+
+    #Adding the phishing labels
+    y = [0]*untrustedWeb
+
+    #Adding the Non phishing labels
+    y.extend([1]*trustedWeb)
+
+    #finding the pattern
+    clf.fit(X, y)
+
+    '''
+    s = pickle.dumps(clf)
+    clf2 = pickle.load(s)
+    print clf2.predict(featureAdd("https://www.google.com"))
+    '''
+
+    #Saving for later purposes
+    joblib.dump(clf, 'learned.pkl')
+
+learning()
