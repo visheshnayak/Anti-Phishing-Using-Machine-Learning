74e94ee5f1a558f01ca1d0b8fea7f189fa662a73
Vishesh Kumar Nayak <visheshnayak@gmail.com>
2017-04-23 12:24:57 +0530

    modified inception.sh

--- inception.sh
+++ inception.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 clear
 
+sudo apt-get install python-pip
+
 sudo pip install selenium
 sudo pip install Flask
+sudo pip install -U scikit-learn
+
 
 echo "After completing this action you can close the terminal."
